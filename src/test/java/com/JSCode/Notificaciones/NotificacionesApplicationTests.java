abc04b0a52a0dc3e6b58e08fc6072269f84f70a1
Juandavid611 <rojasnarvaezjuandavid0611@gmail.com>
2025-06-19 18:33:13 -0500

    Primer commit

---
+++ src/test/java/com/JSCode/Notificaciones/NotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JSCode.Notificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
